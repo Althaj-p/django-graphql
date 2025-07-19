1904a7e771bd4acd6538d38639f39ac934bbfd3d
unknown <muhammedalthaj.primalcodes@gmail.com>
2025-07-19 17:59:38 +0530

    initial commit

---
+++ core/schema.py
@@ -0,0 +1,11 @@
+import graphene
+
+import graphql_api.schema
+
+
+class Query(graphql_api.schema.Query, graphene.ObjectType):
+    # This class will inherit from multiple Queries
+    # as we begin to add more apps to our project
+    pass
+
+schema = graphene.Schema(query=Query)
